c720dc30596ed4389c92b496fa041d00b4495ed7
Daniel Anestopoulos <dstops97@gmail.com>
2023-02-28 22:07:35 -0500

    1-4 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const charArray = string.split('');
+    let reversed = '';
 
+    for (let i = 0; i < charArray.length; i++){
+        reversed += charArray[(charArray.length-1)-i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let restart;
 
+    do{
+        restart = false;
+        for(let i = 0; i < array.length; i++){
+            for (let j = 0; j < args.length; j++)
+            {
+                if (array[i] === args[j]){
+                    array.splice(i,1);
+                    restart = true;
+                }
+            }
+        }
+    } while (restart);
+
+
+    return array;
 };
 
 // Do not edit below this line
