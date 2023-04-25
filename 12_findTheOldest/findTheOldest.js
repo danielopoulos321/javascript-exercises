dd9d69c8272fa8ebe099e3f5be6eb9a3570dbec3
Daniel Anestopoulos <dstops97@gmail.com>
2023-04-24 20:47:17 -0400

    Redone 8-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,21 +1,17 @@
-const findTheOldest = function(people) {
-let oldestAge = 0;
-let oldestPerson;
-    people.forEach(person => {
-        let age;
-        if (person.yearOfDeath == undefined){
-            age = new Date().getFullYear() - person.yearOfBirth;
-        } else {
-            age = person.yearOfDeath - person.yearOfBirth;
-        }
-        
-        if (age > oldestAge){
-            oldestAge = age;
-            oldestPerson = person;
-        }
+const findTheOldest = function (people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return currentAge > oldestAge ? currentPerson : oldest;
     });
-    return oldestPerson;
-};
+}
+
+const getAge = function(birth, death) {
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
